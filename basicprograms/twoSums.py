40b64154783a5da20a6d3cad8ce3d64c2655c23d
Bhargavi Ramalingaiah <bhargavi.ramalingaiah@plexxi.com>
2021-07-12 10:13:01 -0400

    first sameple two sums program

--- basicprograms/twoSums.py
+++ basicprograms/twoSums.py
@@ -1,18 +1,25 @@
-class Solution(object):
+class Solution:
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        # looping through an array and storing the indices in i
+        # declaring the dictionary
+        dict_list = {}
+        # looping through for loop index
         for i in range(len(nums)):
-            # storing the indices from i+1 in j
-            for j in range(i+1, len(nums)):
-                if (nums[i] + nums[j] == target):
-                    return [i, j]
+            # storing the key, values as dict from list
+            dict_list[i] = nums[i]
+            # finding value
+            value = target - dict_list[i]
+            if value in dict_list.values():
+                test = self.getKey(dict_list, value, i)
+                if test != None:
+                    return [test, i]
 
-# call the function
+    # get the keys of dictionary
+    def getKey(self, dict_list, input_value, index):
+        for key, value in dict_list.items():
+            if (input_value == value and index != key):
+                return key;
+
+            # call the function
 output = Solution()
 result = output.twoSum([1,2,3], 3)
 print(result)
